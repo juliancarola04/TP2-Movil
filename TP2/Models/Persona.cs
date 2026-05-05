ac72d06a272cc615e5048c1d75f4344961ba8bfd
Julián Carola <juliancarola04@gmail.com>
2026-05-05 19:41:35 -0300

    Agregar archivos de proyecto.

---
+++ TP2/Models/Persona.cs
@@ -0,0 +1,13 @@
+﻿namespace TP2.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Domicilio { get; set; } = string.Empty;
+        public DateTime FechaNacimiento { get; set; }
+        public string RutaImagen { get; set; } = string.Empty;
+
+    }
+}
