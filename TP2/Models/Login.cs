abc3d33f6db64d36e0bed5cb30331309a302a299
Julián Carola <juliancarola04@gmail.com>
2026-05-05 21:57:06 -0300

    Agregar página de login y eliminar páginas no utilizadas.

---
+++ TP2/Models/Login.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TP2.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "El usuario es obligatorio.")]
+        public string Usuario { get; set; } = string.Empty;
+        [Required(ErrorMessage = "La clave es obligatoria.")]
+        public string Clave { get; set; } = string.Empty;
+    }
+}
